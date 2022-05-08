62b491510f3d621fe1b29d298e9482fbe30f8f1f
Pierre Le Roux <leroux.pierre@gmail.com>
2022-05-08 23:27:31 +0200

    feat: Add rotation and direction - doesn't work

--- game.py
+++ game.py
@@ -41,55 +41,75 @@ def draw_grid():
 
 ASTEROIDS = draw_grid()
 
-def draw_window(player, enemy):
+def draw_window(player, player_rotation, enemy, enemy_rotation):
 
     WIN.fill(BLACK)
     for asteroid in ASTEROIDS:
         WIN.blit(ASTEROID_IMAGE, (asteroid.x, asteroid.y))
+    PLAYER = pygame.transform.rotate(PLAYER, player_rotation)
     WIN.blit(PLAYER, (player.x, player.y))
-    WIN.blit(ENEMY, (enemy.x, enemy.y))
+    WIN.blit(pygame.transform.rotate(ENEMY, enemy_rotation), (enemy.x, enemy.y))
 
     pygame.display.update()
 
-def movement(keys_pressed, player, enemy=False):
+def movement(keys_pressed, direction_current, player, enemy=False):
+    direction_new = direction_current
     if enemy:
         if keys_pressed[pygame.K_a] and player.x - VEL >= 0: # LEFT
             player.x -= VEL
+            direction_new = [1, 0, 0]
         if keys_pressed[pygame.K_d] and player.x + SPACESHIP_WIDTH + VEL <= WIDTH: # RIGHT
             player.x += VEL
+            direction_new = [0, 0, 1]
         if keys_pressed[pygame.K_w] and player.y - VEL >= 0: # UP
             player.y -= VEL
+            direction_new = [0, 1, 0]
         if keys_pressed[pygame.K_s] and player.y + SPACESHIP_HEIGHT + VEL <= HEIGHT: # DOWN
             player.y += VEL
+            direction_new = [0, 0, 0]
     else:
         if keys_pressed[pygame.K_LEFT] and player.x - VEL >= 0: # LEFT
             player.x -= VEL
+            direction_new = [1, 0, 0]
         if keys_pressed[pygame.K_RIGHT] and player.x + SPACESHIP_WIDTH + VEL <= WIDTH: # RIGHT
             player.x += VEL
+            direction_new = [0, 0, 1]
         if keys_pressed[pygame.K_UP] and player.y - VEL >= 0: # UP
             player.y -= VEL
+            direction_new = [0, 1, 0]
         if keys_pressed[pygame.K_DOWN] and player.y  + SPACESHIP_HEIGHT + VEL <= HEIGHT: # DOWN
             player.y += VEL
+            direction_new = [0, 0, 0]
 
     if collide(player):
         if enemy:
             if keys_pressed[pygame.K_a] and player.x - VEL >= 0: # LEFT
                 player.x += VEL
+                direction_new = [1, 0, 0]
             if keys_pressed[pygame.K_d] and player.x + SPACESHIP_WIDTH + VEL <= WIDTH: # RIGHT
                 player.x -= VEL
+                direction_new = [0, 0, 1]
             if keys_pressed[pygame.K_w] and player.y - VEL >= 0: # UP
                 player.y += VEL
+                direction_new = [0, 1, 0]
             if keys_pressed[pygame.K_s] and player.y + SPACESHIP_HEIGHT + VEL <= HEIGHT: # DOWN
                 player.y -= VEL
+                direction_new = [0, 0, 0]
         else:
             if keys_pressed[pygame.K_LEFT] and player.x - VEL >= 0: # LEFT
                 player.x += VEL
+                direction_new = [1, 0, 0]
             if keys_pressed[pygame.K_RIGHT] and player.x + SPACESHIP_WIDTH + VEL <= WIDTH: # RIGHT
                 player.x -= VEL
+                direction_new = [0, 0, 1]
             if keys_pressed[pygame.K_UP] and player.y - VEL >= 0: # UP
                 player.y += VEL
+                direction_new = [0, 1, 0]
             if keys_pressed[pygame.K_DOWN] and player.y  + SPACESHIP_HEIGHT + VEL <= HEIGHT: # DOWN
                 player.y -= VEL
+                direction_new = [0, 0, 0]
+
+    return player_orientation(direction_current, direction_new)
         
 
 def collide(player):
@@ -102,9 +122,42 @@ def collide(player):
 
     return collision
 
+def player_orientation(direction_current, direction_new):
+    '''
+    [0, 0, 1] # right
+    [1, 0, 0] # left
+    [0, 1, 0] # up
+    [0, 0, 0] # down
+    '''
+    if direction_current != direction_new:
+        delta = [
+            direction_current[0] - direction_new[0], 
+            direction_current[1] - direction_new[1], 
+            direction_current[2] - direction_new[2]
+        ]
+
+        if delta in [[0, 1, 0], [0, -1, 0], [1, 0, -1], [-1, 0, 1]]:
+            rotation = 180
+
+        elif delta in [[-1, 1, 0], [1, 0, 0], [0, 0, -1], [0, -1, 1]]:
+            rotation = -90
+
+        elif delta in [[0, 1, -1], [0, 0, 1], [-1, 0, 0], [1, -1, 0]]:
+            rotation = 90
+
+        direction = direction_new
+    else:
+        rotation = 0
+        direction = direction_current
+
+    return rotation, direction
+
 def main():
     player = pygame.Rect(WIDTH - SPACESHIP_WIDTH, HEIGHT - SPACESHIP_HEIGHT, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
     enemy = pygame.Rect(0, 0, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
+    player_dir = [1, 0, 0]
+    enemy_dir = [0, 0, 0]
 
     clock = pygame.time.Clock()
     run = True
@@ -115,10 +168,12 @@ def main():
                 run = False
 
         keys_pressed = pygame.key.get_pressed()
-        movement(keys_pressed, player)
-        movement(keys_pressed, enemy, True)
+        player_rotation, player_dir = movement(keys_pressed, player_dir, player)
+        enemy_rotation, enemy_dir = movement(keys_pressed, enemy_dir, enemy, True)
 
-        draw_window(player, enemy)
+        ic(player_rotation, player_dir)
+
+        draw_window(player, player_rotation, enemy, enemy_rotation)
 
 
     
